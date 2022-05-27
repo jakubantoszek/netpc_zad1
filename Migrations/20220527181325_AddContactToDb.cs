e2853ee644a54d0cc669303ab7fd0860428336ea
jakubantoszek <102921899+jakubantoszek@users.noreply.github.com>
2022-05-28 00:07:01 +0200

    Add files via upload

---
+++ Migrations/20220527181325_AddContactToDb.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ContactList.Migrations
+{
+    public partial class AddContactToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Contact",
+                columns: table => new
+                {
+                    email = table.Column<string>(nullable: false),
+                    name = table.Column<string>(nullable: false),
+                    surname = table.Column<string>(nullable: false),
+                    password = table.Column<string>(nullable: false),
+                    category = table.Column<int>(nullable: false),
+                    subcategory = table.Column<string>(nullable: true),
+                    phoneNumber = table.Column<string>(maxLength: 20, nullable: false),
+                    date = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contact", x => x.email);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contact");
+        }
+    }
+}
